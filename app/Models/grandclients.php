4a2f233872d7f429e84aa867b0d8871300a03647
ArtilleryJuggernot <artillery@artillery.fr>
2024-02-15 15:49:39 +0100

    Model OK | Test dashboard dao OK

---
+++ app/Models/grandclients.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class grandclients extends Model
+{
+    use HasFactory;
+    protected $table = "grandclients";
+}
---
+++ app/Models/ligne_facturation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ligne_facturation extends Model
+{
+    use HasFactory;
+    protected $table = "ligne_facturation";
+}
